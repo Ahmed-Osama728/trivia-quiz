da7737d8b5efe3438700499bac8db9fcf1cba3a0
Ahmed-Osama728 <ahmedosama12896@gmail.com>
2024-05-23 14:07:23 +0300

    Feat: add the Base layout

---
+++ trivia-quiz/src/components/layouts/BaseLayout.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const StyledContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background: linear-gradient(163.34deg, #131339 11.49%, #00ac97 125.44%);
+`;
+
+const BaseLayout = ({ children }) => {
+  return <StyledContainer>{children}</StyledContainer>;
+};
+
+export default BaseLayout;
