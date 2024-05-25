84aaeafdbbbbfef0cb9277379942aa2317ea6f84
Ahmed-Osama728 <ahmedosama12896@gmail.com>
2024-05-25 21:58:58 +0300

    feat: add the score page component route

--- trivia-quiz/src/App.jsx
+++ trivia-quiz/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home/Home';
 import BaseLayout from './components/layouts/BaseLayout';
 import QuestionsCategories from './pages/QuestionsCategories/QuestionsCategories';
 import Question from './pages/Questions/Questions';
+import Score from './pages/Score/Score';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/categories" element={<QuestionsCategories />} />
           <Route path="/question" element={<Question />} />
+          <Route path="/score" element={<Score />} />
         </Routes>
       </BaseLayout>
     </Router>
